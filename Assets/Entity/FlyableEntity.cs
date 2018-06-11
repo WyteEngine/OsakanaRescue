c8ed9197833e99369c7a54a79d0851e0207714f4
Xeltica <citringo@gmail.com>
2018-06-11 22:38:44 +0900

    Add FlyableEntity

---
+++ Assets/Entity/FlyableEntity.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public abstract class FlyableEntity : LivableEntity
+{
+	public override string WalkAnimationId => FlyAnimationId;
+
+	public override string JumpAnimationId => null;
+
+	public override string LandSfxId => null;
+
+	public override string JumpSfxId => null;
+
+	/// <summary>
+	/// 飛行中のアニメーションを取得します．
+	/// </summary>
+	/// <returns></returns>
+	public abstract string FlyAnimationId { get; }
+
+	/// <summary>
+	/// このEntityの現在の飛行状態を取得または設定します．このプロパティに応じて，適切なアニメーションが行われます．
+	/// </summary>
+	/// <returns></returns>
+	public FlyableEntityState State { get; set; }
+
+	public enum FlyableEntityState
+	{
+		/// <summary>
+		/// 飛行中．
+		/// </summary>
+		Fly,
+		/// <summary>
+		/// 待機中．
+		/// </summary>
+		Stay
+	}
+
+	/// <summary>
+	/// EntityLiving から継承されるメソッドです．単純に，速度ベクトルのX座標を指定します．
+	/// </summary>
+	/// <param name="rightSpeed">速度ベクトルのX座標を指定します．</param>
+	/// <param name="hold">意味を持ちません．</param>
+	public override void Move(float rightSpeed, bool hold = true)
+	{
+		Move(new Vector2(rightSpeed, 0));
+	}
+
+	/// <summary>
+	/// 指定した値を速度として移動します．
+	/// </summary>
+	/// <param name="speed"></param>
+	public void Move(Vector2 speed)
+	{
+		Velocity = speed;
+		if (speed != Vector2.zero)
+			State = FlyableEntityState.Fly;
+	}
+
+	/// <summary>
+	/// その場で停止します．
+	/// </summary>
+	public void Stop()
+	{
+		State = FlyableEntityState.Stay;
+		// とまる
+		Move(Vector2.zero);
+	}
+
+	// 飛行可能なので重力影響はない
+	public override float GravityScale => 0;
+}
