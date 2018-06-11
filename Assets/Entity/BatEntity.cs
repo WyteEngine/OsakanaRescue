2f61d0dc6f77102ac4bb964d2ee1a959d9832c40
Xeltica <citringo@gmail.com>
2018-06-11 22:38:56 +0900

    Add BatEntity

---
+++ Assets/Entity/BatEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatEntity : FlyableEntity
+{
+	public override string StayAnimationId => "entity.enemy.bat.stay";
+
+	public override string DeathSfxId => null;
+
+	public override string FlyAnimationId => "entity.enemy.bat.fly";
+}
