30c6bd457232d6b45aaa06b730446e308476f34f
Xeltica <citringo@gmail.com>
2018-06-28 12:18:39 +0900

    ♥

---
+++ Assets/Entity/HeartEntity.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+/// <summary>
+/// ヒーリングアイテム．
+/// </summary>
+[RequireComponent(typeof(AIWalkAndWrapOnTheWall))]
+public class HeartEntity : LivableEntity
+{
+	public override string WalkAnimationId => heartAnimId;
+
+	public override string StayAnimationId => heartAnimId;
+
+	public override string JumpAnimationId => heartAnimId;
+
+	public override string LandSfxId => null;
+
+	public override string JumpSfxId => null;
+
+	public override string DeathSfxId => null;
+
+	const string heartAnimId = "entity.heart";
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+		if (IsCollidedWithPlayer())
+		{
+			Sfx.Play("entity.player.heal");
+			Wyte.CurrentPlayer.Heal(this, 1);
+			// 自殺しておく
+			Kill(Wyte.CurrentPlayer);
+		}
+	}
+}
