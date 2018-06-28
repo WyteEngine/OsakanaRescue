8461a5250d906cf7ad90e5eb8b50ff616c735bc8
Xeltica <citringo@gmail.com>
2018-06-28 09:04:42 +0900

    Add BallEntity

---
+++ Assets/Entity/BallEntity.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AISteppableEnemy))]
+public class BallEntity : LivableEntity
+{
+	public override string WalkAnimationId => ballAnimationId;
+
+	public override string StayAnimationId => ballAnimationId;
+
+	public override string JumpAnimationId => ballAnimationId;
+
+	public override string LandSfxId => null;
+
+	public override string JumpSfxId => "entity.ball.bounce";
+
+	public override string DeathSfxId => null;
+
+	public BulletEntity Parent { get; set; }
+
+	private string ballAnimationId;
+
+	protected override void Start()
+	{
+		base.Start();
+		ballAnimationId = $"entity.ball.{Random.Range(0, 3)}";
+		Velocity = new Vector2(Wyte.CurrentPlayer != null && Wyte.CurrentPlayer.transform.position.x < transform.position.x ? -80 : 80, 0);
+	}
+
+	protected override void OnUpdate()
+	{
+		// はね続ける
+		Jump();
+
+		if (CanKickLeft() || CanKickRight())
+		{
+			Kill(this);
+		}
+	}
+
+	protected override IEnumerator OnDeath(Object killer)
+	{
+		return base.OnDeath(killer);
+	}
+
+}
