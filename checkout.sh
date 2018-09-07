ab49985736c09af39b2055ef17e59573c54bc56c
Xeltica <citringo@gmail.com>
2018-09-07 11:50:35 +0900

    Update checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -2,5 +2,9 @@
 
 cd Assets/Wyte/
 git config core.sparsecheckout true
+if [ ! -d ../../.git/modules/wyte/info ]; then
+	mkdir ../../.git/modules/wyte/info
+fi
+
 echo "Assets/Wyte" > ../../.git/modules/wyte/info/sparse-checkout
 git read-tree -mu HEAD
