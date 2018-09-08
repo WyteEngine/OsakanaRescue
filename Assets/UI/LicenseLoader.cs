5bfd4e1793a5f1b77c49e388e2e309790d99d5ee
Xeltica <citringo@gmail.com>
2018-09-08 18:47:25 +0900

    メニュー用のアセット追加

---
+++ Assets/UI/LicenseLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Text))]
+public class LicenseLoader : MonoBehaviour
+{
+	[SerializeField]
+	TextAsset licenseText;
+
+	Text ui;
+
+	// Use this for initialization
+	void Start()
+	{
+		ui = GetComponent<Text>();
+	}
+
+	private void Update()
+	{
+		if (ui == null)
+			ui = GetComponent<Text>();
+		if (licenseText != null && ui.text != licenseText.text)
+			ui.text = licenseText.text;
+	}
+}
---
+++ Assets/UI/VersionText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WyteEngine.UI
+{
+	[RequireComponent(typeof(Text))]
+	public class VersionText : BaseBehaviour
+	{
+		Text ui;
+		// Use this for initialization
+		void Start()
+		{
+			ui = GetComponent<Text>();
+			ui.text = $"Version: {Wyte.GameVersion}\nWyte Version: {Wyte.LongVersion}";
+		}
+	}
+}
