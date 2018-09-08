c51ca47dba05d792b29e8cf6854528f666ae7183
Xeltica <citringo@gmail.com>
2018-09-08 18:43:49 +0900

    端末のタイプによってGameObjectを非表示させる、ヘルパーを追加

---
+++ Assets/Utility/Hidden.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WyteEngine.Utility
+{
+	public class Hidden : BaseBehaviour {
+
+		[SerializeField]
+		HiddenFlag hiddenOn = HiddenFlag.SmartDevice;
+
+		// Use this for initialization
+		void Start() {
+			if ((hiddenOn == HiddenFlag.SmartDevice && IsSmartDevice) || (hiddenOn == HiddenFlag.PC && !IsSmartDevice))
+				gameObject.SetActive(false);
+		}
+
+
+		public enum HiddenFlag
+		{
+			PC, SmartDevice
+		}
+	}
+}
